5a7f5f094749d2e9e69b528264f024ac6c126922
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-02-02 20:19:44 +0530

    31 complete

---
+++ Assets/MiltiPlayerScript/MaitiPlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaitiPlayerMovement : MonoBehaviour
+{
+    public int DisplayScore;
+    public float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        float horizontal;
+        float vertical;
+
+        if (gameObject.tag == "Player1")
+        {
+            horizontal = Input.GetAxis("Horizontal1");
+            vertical = Input.GetAxis("Vertical1");
+        }
+        else if (gameObject.tag == "Player2")
+        {
+            horizontal = Input.GetAxis("Horizontal2");
+            vertical = Input.GetAxis("Vertical2");
+        }
+        else
+        {
+            return;
+        }
+
+        Vector2 direction = new Vector2(horizontal, vertical);
+        transform.position = transform.position + (Vector3)(direction * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ebulat"))
+        {
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+    public void UpdateScor(int DethCount)
+    {
+        DisplayScore += DethCount;
+        if (DisplayScore == 2)
+        {
+            speed *= 2;
+        }
+    }
+}
---
+++ Assets/MiltiPlayerScript/MitlyPlayerShoot.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MitlyPlayerShoot : MonoBehaviour
+{
+
+    [SerializeField] private GameObject bulletPrefab;
+    public float bulletSpeed = 10f;
+    private AudioSource audioSource;
+    public AudioClip playerShootSound;
+    public Vector3 mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+        mousePos = Input.mousePosition;
+    }
+    private void Shoot()
+    {
+        if (gameObject.tag == "Player1")
+        {
+            if (Input.GetKey(KeyCode.RightControl))
+            {
+                print("Shoot palyer1");
+                audioSource.clip = playerShootSound;
+                audioSource.Play();
+
+                GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                bullet.GetComponent<Rigidbody2D>().velocity = transform.position * bulletSpeed;
+
+                Destroy(bullet, 2f);
+
+            }
+
+
+        }
+       else if (gameObject.tag == "Player2")
+        {
+            if (Input.GetKey(KeyCode.LeftControl))
+
+            {
+                print("Shoot palyer2");
+                audioSource.clip = playerShootSound;
+                audioSource.Play();
+
+                GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                bullet.GetComponent<Rigidbody2D>().velocity = transform.position * bulletSpeed;
+                Destroy(bullet, 2f);
+
+            }
+
+
+        }
+
+    }
+}
